3764977c6b71a367a0b010f87e4a384207030a75
zhangbinwen <2584197232@qq.com>
2016-08-22 08:14:49 +0800

    add server part node.js code -- server.js

---
+++ server-node.js-code/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.emit('connected', '你连我干嘛');
+});
+
+http.listen(3000, function() {
+	console.log('listening on : 3000')
+});
